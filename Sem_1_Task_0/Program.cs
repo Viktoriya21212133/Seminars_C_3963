993464e37db2ee0f06380496eacd8695d2bf18c3
Viktoriya <shakhova_v@bk.ru>
2023-02-10 23:33:01 +0300

    Напишите программу, которая на вход принимает число и выдает его квадрат

---
+++ Sem_1_Task_0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+
+//Приглашение на ввод числа
+Console.Write("Enter your number: ");
+
+// считывает данные с кода 
+string? inputNum = Console.ReadLine ();
+// Проверяем, чтобы данные не были пустыми
+if (inputNum != null)
+{
+   //Парсим введенное число
+    int number = int.Parse(inputNum);
+
+
+// Находим квадрат числа
+int outNum = number*number;
+
+// Выводим данные в консоль
+Console.Write ("Square number: ");
+Console.Write (outNum);
+}
+
