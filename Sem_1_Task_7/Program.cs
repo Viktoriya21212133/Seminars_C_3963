faf8758c05247e5cf3a247c63c1e36edc306aefe
Viktoriya <shakhova_v@bk.ru>
2023-02-13 22:12:01 +0300

    Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа

---
+++ Sem_1_Task_7/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+
+// Приглашение на ввод числа
+Console.WriteLine ("Enter number of threes: ");
+
+// Считываем число 
+string? LineN = Console.ReadLine ();
+
+// Парсим введенное число
+if (LineN !=null)
+{
+    int number = int.Parse (LineN);
+      
+if (number > 100 && number < 1000)
+{
+    int res = number%10;
+    Console.WriteLine ("last number:"+ res);
+}
+else
+{
+    Console.WriteLine ("Error!");
+}
+    //Console.WriteLine ("last number:" + res);
+}
+
