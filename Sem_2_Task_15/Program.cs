5ddfeeef3296ee812e84c83b061e1fc4975a6806
Viktoriya <shakhova_v@bk.ru>
2023-02-19 01:53:28 +0300

    Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет является ли она выходным дне, используя конструкцию Dictionary

---
+++ Sem_2_Task_15/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет является ли она выходным днем
+// *Использовать конструкцию Dictionary
+
+// Пишем словарь
+Dictionary <int, string> weekday = new Dictionary <int, string>();
+
+weekday.Add (1, "weekday - Monday");
+weekday.Add (2, "weekday - Tuesday");
+weekday.Add (3, "weekday - Wednesday");
+weekday.Add (4, "weekday - Thursday");
+weekday.Add (5, "weekday - Friday");
+weekday.Add (6, "day off - Saturday");
+weekday.Add (7, "day off - Sunday");
+
+// Приглашение на ввода числа
+Console.Write("Enter your number: ");
+//Парсим число
+string? days = Console.ReadLine();
+if (days!=null)
+{
+int day = Int32.Parse(days);
+ // Проверяем значения
+    if (weekday.ContainsKey (day))
+    {
+    Console.WriteLine (weekday[day]);
+    }
+    else
+    {
+    Console.WriteLine ("Is not day of week");
+    }
+    
+}
+
+
+
