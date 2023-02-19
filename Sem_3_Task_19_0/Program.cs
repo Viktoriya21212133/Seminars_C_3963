62fe645f631f73d820c46d86de00fdc9b147237b
Viktoriya <shakhova_v@bk.ru>
2023-02-19 20:48:43 +0300

    Напишите программу, которая принимает на вход пятизначное значениек и проверяет является ли оно палиндромомом

---
+++ Sem_3_Task_19_0/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное значение и проверяет является ли оно палиндромом
+
+// Водим пятизначное число для его проверки и сразу его парсим!
+Console.WriteLine ("Inter five-digit number: ");
+int Number = int.Parse(Console.ReadLine()??"0"); 
+
+//Находим первое число
+int FirstNumber = (Number/10000)%10;
+//Console.WriteLine (FirstNumber);
+//Находим второе число
+int SecondNumber = (Number/10) % 10;
+//Console.WriteLine (SecondNumber);
+//Находим четвертое число
+int FourthNumber = (Number/10) % 10;
+//Console.WriteLine (FourthNumber);
+//Находим пятое число
+int FifthNumber = Number % 10;
+//Console.WriteLine (FifthNumber);
+//Начинаем проверку для начала на пятизначность 
+if (Number>10000 && Number<100000)
+{
+    // Пошли в цикл проверки на палиндром
+        if (FirstNumber == FifthNumber && FourthNumber == SecondNumber)
+        Console.WriteLine ("This number is palindome");
+        else
+        {
+        Console.WriteLine ("This number is not palindrome");
+        }
+} 
+else
+Console.WriteLine ("This number is not five-digit number");
