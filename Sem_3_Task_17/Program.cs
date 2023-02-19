983bcc67401613caa40374e00969d3e1737cedad
Viktoriya <shakhova_v@bk.ru>
2023-02-19 21:59:12 +0300

    Напишите программу, которая принимает на вход координаты точки (х,у), причем х не равно у, и выдает номер четверти плоскости, в которой находится эта точка

---
+++ Sem_3_Task_17/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (х,у), причем х не равно у, и выдает номер четверти плоскости, в которой находится эта точка
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.WriteLine (message);
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Объявляем переменные
+int CoordX = ReadData ("Enter X: ");
+int CoordY = ReadData ("Enter Y: ");
+
+// Метод, который ничего не вернет, но определит четверть
+void PrintQuarterTest(int X, int Y)
+{
+    if (X > 0 && Y > 0) Console.WriteLine ("first quarter point");
+    if (X > 0 && Y < 0) Console.WriteLine ("second quarter point");
+    if (X < 0 && Y > 0) Console.WriteLine ("third quarter point");
+    if (X < 0 && Y < 0) Console.WriteLine ("fourth quarter point");
+}
+
+// Обращаемся к методу
+PrintQuarterTest (CoordX, CoordY);
