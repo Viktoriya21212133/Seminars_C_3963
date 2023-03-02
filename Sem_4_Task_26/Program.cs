683bb0f7b28eb17c04c39ee34231e3dd79c0d68d
Viktoriya <shakhova_v@bk.ru>
2023-03-02 20:46:44 +0300

    Напишите программу, которая принимает на вход число и выдает количество цифр в числе

---
+++ Sem_4_Task_26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// метод вывода результатов
+void PrintResult (string message)
+{
+    Console.WriteLine (message);
+}
+// Метод расчета через десятичный логарифм
+int NumberOffDigits (int num)
+{
+    return (int) (Math.Floor (Math.Log10 (num)))+1;
+}
+
+// Объявление переменных для методов
+int num = ReadData ("Enter number A: ");
+int num1 = NumberOffDigits (num);
+
+// Вызов методов
+
+PrintResult ("Enter your number = "+num1);
+
