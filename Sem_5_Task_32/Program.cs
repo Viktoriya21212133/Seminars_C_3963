9769830d81832b7e6da85c8d2007e206b255a883
Viktoriya <shakhova_v@bk.ru>
2023-03-03 15:59:52 +0300

    Напишите программу замены элементов массива. Положительные элементы массива заменить на отрицательные и наоборот

---
+++ Sem_5_Task_32/Program.cs
@@ -0,0 +1,77 @@
+﻿// Напишите программу замены элементов массива. Положительные элементы массива заменить на отрицательные и наоборот
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод печати массива 
+void Print1DArray (int [] array)
+{
+    Console.Write ("Generated array [");
+        for (int i = 0; i <array.Length-1; i++)
+        {
+            Console.Write (array[i] +", ");
+        }
+    Console.WriteLine (array[array.Length-1]+"]");
+}
+
+void Print1DArrayInvers (int [] array)
+{
+    Console.Write ("Invers array [");
+        for (int i = 0; i <array.Length-1; i++)
+        {
+            Console.Write (array[i] +", ");
+        }
+    Console.WriteLine (array[array.Length-1]+"]");
+}
+
+// Метод генерации массива с методом исключения ошибки min/max
+int [] Gen1DArray (int Length, int min, int max)
+{
+    if (min>max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int [] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min,max+1);
+    }
+    return array;
+}
+
+// Метод смены знака
+void InversArr (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i]=array[i]*(-1);
+}
+
+
+// Обращение к методу ввода
+int Length = ReadData ("Enter Lenght massiv: ");
+int min = ReadData ("Enter min massiv: ");
+int max = ReadData ("Enter max massiv: ");
+
+// Вывод сгенерированного массива
+int []arr = Gen1DArray(Length,min,max);
+Print1DArray (arr);
+
+// Вывод результатов
+InversArr (arr);
+Print1DArrayInvers (arr);
+
+// Время, затраченное на метод 
+Console.Write ("Elapsed time = ");
+DateTime d1 = DateTime.Now;
+Console.WriteLine (DateTime.Now - d1);
+
+
+
