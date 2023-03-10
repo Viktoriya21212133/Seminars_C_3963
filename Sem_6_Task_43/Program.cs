f56750861afc7d9974249981f4be0a4a73cb717d
Viktoriya <shakhova_v@bk.ru>
2023-03-10 18:39:51 +0300

    Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями y = k1*x+b1 и y = k2*x+b2, при этом значения k1, b1, k2, b2 задает пользователь

--- Sem_6_Task_43/Program.cs
+++ Sem_6_Task_43/Program.cs
@@ -9,20 +9,16 @@ int ReadData (string message)
 }
 
 // Метод проверки
-(double resX, double resY) Intersectoin (int k1, int b1, int k2, int b2)
+(double resX, double resY) Intersectoin (double k1, double b1, double k2, double b2)
 {
-    double y= 0;
-    //double x= new double();
-    //x = (y-b1)/k1;
-    //y = k2*x+b2;
-    double x = ((k2*y-k2*b1)/k1)+b2;
-    y = (x-b1)/k1;
+    double x = (b2-b1)/(k1-k2);
+    double y = (k1*b2 - k2*b1)/(k1-k2);
 return (x, y);
 }
-int k1 = ReadData ("Enter k1: ");
-int b1 = ReadData ("Enter b1: ");
-int k2 = ReadData ("Enter k2: ");
-int b2 = ReadData ("Enter b2: ");
+double k1 = ReadData ("Enter k1: ");
+double b1 = ReadData ("Enter b1: ");
+double k2 = ReadData ("Enter k2: ");
+double b2 = ReadData ("Enter b2: ");
 
 (double resultX, double resultY) ress= Intersectoin (k1, b1, k2, b2);
 Console.WriteLine (ress);
