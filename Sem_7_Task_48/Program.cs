af450130ae77978c8898066094e9ea717d241204
Viktoriya <shakhova_v@bk.ru>
2023-03-10 18:42:02 +0300

    Задайте двумерный массив, размером m*n каждый элемент в массиве находится по формуле A(m.n) = m+n. Выведите полученный массив на экран

---
+++ Sem_7_Task_48/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив, размером m*n каждый элемент в массиве находится по формуле A(m.n) = m+n. Выведите полученный массив на экран 
+
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод заполнения массива
+int [,] Gen2DArrayMod (int countRow, int countColumn, int min, int max)
+{
+    if (min>max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int [,] array = new int[countRow,countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        array[i,j] = i+j;
+    }
+    return array;
+}
+
+// Метод вывода массива
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i <array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+    Console.WriteLine ();
+    }
+}
+
+// Вводим данные 
+int countRow = ReadData ("Enter the number of Row: ");
+int countColumn = ReadData ("Enter the number of columns: ");
+int min = ReadData ("Enter min massiv: ");
+int max = ReadData ("Enter max massiv: ");
+int [,] array2D = Gen2DArrayMod (countRow, countColumn, min, max);
+Print2DArray (array2D);
