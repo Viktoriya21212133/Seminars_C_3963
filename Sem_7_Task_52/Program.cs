dd8edcab0dcadd313b2a948a4b2fc221e4e69675
Viktoriya <shakhova_v@bk.ru>
2023-03-10 18:46:35 +0300

    Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце

---
+++ Sem_7_Task_52/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод заполнения массива
+int [,] Gen2DArray (int countRow, int countColumn, int min, int max)
+{
+    if (min>max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int [,] array = new int[countRow,countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        array[i,j] = rnd.Next(min,max+1);
+    }
+    return array;
+}
+
+// Метод вывода массива
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i <array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+    Console.WriteLine ();
+    }
+}
+
+//Метод подсчета среднего арифметического
+void Average2DArray (int [,] array)
+{
+    for (int i = 0; i< array.GetLength(0); i++)
+    {
+        for (int j = 0; j< array.GetLength(1); j++)
+            System.Console.Write($"{array[i,j],4}");   
+    } 
+}
+
+// Вводим данные 
+int countRow = ReadData ("Enter the number of Row: ");
+int countColumn = ReadData ("Enter the number of columns: ");
+int min = ReadData ("Enter min massiv: ");
+int max = ReadData ("Enter max massiv: ");
+
+// Вывод сгенерированного массива
+int [,] array2D = Gen2DArray (countRow, countColumn, min, max);
+Print2DArray (array2D);
+
+// 
+Average2DArray(array2D);
+Console.WriteLine ();
+
+
