ca9bf46cf7ebdf1ad83ca071a6225901b67c8894
Viktoriya <shakhova_v@bk.ru>
2023-03-10 11:32:42 +0300

    Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 он ввел

---
+++ Sem_6_Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 он ввел
+
+Console.Write("Enter your numbers: ");
+// Приглашение на ввод чисел
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//Метод проверки  
+int PositiveNumbers (int [] numbers)
+{
+    int count = 0;
+    for (int i = 0; i<numbers.Length; i++)
+    {
+        if (numbers[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count = PositiveNumbers (numbers);
+Console.WriteLine ("number of positive numbers = "+ count);
