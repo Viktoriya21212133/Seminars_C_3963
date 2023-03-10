b6184b516564d4aeb6a8b4aaa4730ccfe46e597a
Viktoriya <shakhova_v@bk.ru>
2023-03-10 11:33:07 +0300

    Напишите программу, которая будет переводить введенное число из десятичной в двоичную систему исчесления

---
+++ Sem_6_Task_42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая будет переводить введенное число из десятичной в двоичную систему исчесления
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод, который ничего не вернет, но выведет результат
+void PrintData (string message)
+ {
+    Console.WriteLine (message);
+ }
+
+
+// Метод перевода
+string DecToBin (int num)
+{
+    string res = string.Empty;
+    while (num>0)
+    {
+        res = num%2+res;
+        num = num/2;
+    }
+return res;    
+}
+
+int num = ReadData ("Enter number: ");
+string result = DecToBin (num);
+PrintData ("Number in binary = "+ result);
