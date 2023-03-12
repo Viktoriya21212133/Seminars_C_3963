dd278acf9793623faa84e857dc66b242ece868c8
Viktoriya <shakhova_v@bk.ru>
2023-03-13 01:27:23 +0300

    Выведите первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника.

---
+++ Sem_8_Task_61/Program.cs
@@ -0,0 +1,47 @@
+﻿// Выведите первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника. 
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// n! k!*(n-k)!
+//       1
+//      1 1
+//     1 1 1
+
+// Метод расчета факториала
+long Factorial (int n)
+{
+    long res = 1;
+    for (int i =2; i<=n; i++)
+    {
+        res = res*i;
+    }
+    return res;
+}
+
+//Метод вывода на экран
+void PrintPaskaleTriangle (int nRow)
+{
+    for (int i=0; i<nRow; i++)
+    {
+        for (int k = 0; k<nRow-i; k++)
+        {
+            Console.Write (" ");
+        }
+        for (int j=0; j<=i;j++)
+        {
+        Console.Write (" ");
+        Console.Write (Factorial(i)/(Factorial(j)*Factorial(i-j)));
+        }
+        Console.WriteLine ();
+    }
+}
+
+// Обращение к методу печати
+int countRow = ReadData ("Enter the number of lines triangle Pascal: ");
+PrintPaskaleTriangle (countRow);
