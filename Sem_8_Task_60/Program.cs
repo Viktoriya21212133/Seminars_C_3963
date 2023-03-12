044fc857db0564644a52a201fde0a1d27e6868a6
Viktoriya <shakhova_v@bk.ru>
2023-03-13 01:26:49 +0300

    Сформируйте терхмерный массив из неповторяющихся двухзначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каэждого элемента

---
+++ Sem_8_Task_60/Program.cs
@@ -0,0 +1,72 @@
+﻿// Сформируйте терхмерный массив из неповторяющихся двухзначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каэждого элемента
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Ввод данных
+int x = ReadData("Введите X: ");
+int y = ReadData("Введите Y: ");
+int z = ReadData("Введите Z: ");
+
+// Метод генерации массива
+void Gen3DArray (int[,,] array3D)
+{
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++)
+  {
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    {
+      for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+// Метод печати массива
+void Print3DArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+// Объявляем и печатаем массив для работы
+int[,,] array3D = new int[x, y, z];
+Gen3DArray(array3D);
+Print3DArray(array3D);
