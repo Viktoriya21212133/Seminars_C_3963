13621642b4d8084c1ce296180c3d260332d3caa5
Viktoriya <shakhova_v@bk.ru>
2023-03-13 01:24:58 +0300

    Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент во входных данных

---
+++ Sem_8_Task_57/Program.cs
@@ -0,0 +1,82 @@
+﻿// Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент во входных данных
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод заполнения массива
+int [,] Gen2DArray (int countRow, int countColumn, int min, int max)
+{
+    if (min>max)
+    {
+        int buf = min;
+        min = max;
+        max = buf;
+    }
+    Random rnd = new Random();
+    int [,] array = new int[countRow,countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        array[i,j] = rnd.Next(min,max+1);
+    }
+    return array;
+}
+
+// Метод вывода массива
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i <array.GetLength(0); i++)
+    {
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+    Console.WriteLine ();
+    }
+}
+
+// Метод печати одномерного массива
+void Print1DArray (int [] array)
+{
+    Console.Write ("Dictionary[");
+        for (int i = 0; i <array.Length-1; i++)
+        {
+            Console.Write (array[i] +", ");
+        }
+    Console.WriteLine (array[array.Length-1]+"]");
+}
+
+// Метод заполнения словаря
+int [] FreqDicBuild (int [,] array, int len)
+{
+    int [] dic = new int [len];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                dic [array[i,j]]++;
+            }
+        }   
+return dic;        
+}
+
+// Вводим данные 
+int countRow = ReadData ("Enter the number of Row: ");
+int countColumn = ReadData ("Enter the number of columns: ");
+int min = ReadData ("Enter min massiv: ");
+int max = ReadData ("Enter max massiv: ");
+int leng = ReadData ("Enter dictionary length: ");
+
+//Вывод сгенерированного массива
+int [,] array2D = Gen2DArray (countRow, countColumn, min, max);
+Print2DArray (array2D);
+Console.WriteLine ();
+
+// обращение к методу
+int [] lenRes = FreqDicBuild(array2D,leng);
+Print1DArray (lenRes);
