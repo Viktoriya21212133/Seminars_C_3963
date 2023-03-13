ca554ad7744cd698f27372cb86c670956206cd25
Viktoriya <shakhova_v@bk.ru>
2023-03-13 03:54:25 +0300

    Задайте значение N и напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии

---
+++ Sem_9_Task_63/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N и напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// Метод генерации рекурсии
+void LineGenRec (int num)
+{
+    Console.Write (num + " ");
+    if (num == 1)
+    {
+
+    }
+    else
+    {
+        LineGenRec (num-1);
+    }
+} 
+
+// Запуск программы 
+int number = ReadData ("Enter your number: ");
+LineGenRec (number);
