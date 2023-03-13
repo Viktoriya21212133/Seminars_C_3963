b508d4d66208581c607f2e74f171e64231ddb75e
Viktoriya <shakhova_v@bk.ru>
2023-03-13 03:56:23 +0300

    Напишите программу, которая принимает на вход число и возвращает сумму цифр этого числа

---
+++ Sem_9_Task_67/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и возвращает сумму цифр этого числа
+
+// Метод, который считывает данные от пользователя
+int ReadData (string message)
+{
+    Console.Write (message);
+    // Считываем число и парсим его 
+    return int.Parse (Console.ReadLine()??"0");
+}
+
+// метод вывода результатов
+void PrintResult (int message)
+{
+    Console.WriteLine (message);
+}
+
+// функция Аккермана
+int RecSumDig (int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    else 
+    {
+        return num%10+RecSumDig(num/10);
+    }
+    
+}
+// Вызов метода
+int number = ReadData ("Enter your number m: ");
+int result = RecSumDig (number);
+PrintResult (result);
